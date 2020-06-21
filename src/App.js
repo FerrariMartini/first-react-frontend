280cd41ebd98b22fd0763c2953cad60cf5ba24f5
Ivan Martini <ivan.martini@extremedigital.com.br>
2020-06-20 22:41:59 -0300

    handle states, add and delete items of list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import "./styles.css";
+import api from "../src/services/api"
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then(response => {
+      setRepositories(response.data)
+    })
+
+  }, [repositories]);
+
+
   async function handleAddRepository() {
-    // TODO
+    const newRepo = {
+      title: "REPO Blockchain - FrontEnd React",
+      url: "https://github.com.br/API",
+      techs: ["Blockchains", "hyperledger", "ethereum"]
+    };
+
+    const response = await api.post("repositories", newRepo);
+
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`);
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repo =>
+          <li key={repo.id}>
+            {repo.title}<button onClick={() => handleRemoveRepository(repo.id)}>Remover</button>
+          </li>
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
